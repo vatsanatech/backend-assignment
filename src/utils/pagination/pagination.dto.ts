5c0cba3ee3bec3c97a2033dfe703753742753c4c
sumantKr <sumantkumar17200@gmail.com>
2024-10-14 01:38:56 +0530

    add swagger support for pagination

--- src/utils/pagination/pagination.dto.ts
+++ src/utils/pagination/pagination.dto.ts
@@ -1,13 +1,16 @@
 import { IsNumber, IsOptional, Max, Min } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class PaginationDto {
+  @ApiProperty()
   @IsOptional()
   @Type(() => Number)
   @IsNumber()
   @Min(1)
   page: number = 1; // Default page is 1
 
+  @ApiProperty()
   @IsOptional()
   @Type(() => Number)
   @IsNumber()
