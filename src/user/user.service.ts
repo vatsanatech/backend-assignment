6d0bb0e8e6393c7a456bcc9fec57d756318b5f70
Jibran Shaikh <jibrans098@protonmail.com>
2024-11-24 22:20:23 +0530

    reindent lines with 4 spaces

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,39 +6,39 @@ import { UserListDto } from "./dto/user.dto";
 
 @Injectable()
 export class UserService {
-  constructor(
-    @InjectModel(User.name) private userModel: Model<User>,
-    @InjectModel(UserList.name) private userListModel: Model<UserList>
-  ) { }
+    constructor(
+        @InjectModel(User.name) private userModel: Model<User>,
+        @InjectModel(UserList.name) private userListModel: Model<UserList>
+    ) { }
 
-  async addToList(userListDto: UserListDto) {
-    const userExists = await this.userModel.findById(userListDto.userId).select({ _id: 1 }).exec();
+    async addToList(userListDto: UserListDto) {
+        const userExists = await this.userModel.findById(userListDto.userId).select({ _id: 1 }).exec();
 
-    if (!userExists) {
-      return null;
+        if (!userExists) {
+            return null;
+        }
+
+        const userList = new this.userListModel(userListDto);
+        return userList.save();
     }
 
-    const userList = new this.userListModel(userListDto);
-    return userList.save();
-  }
-
-  async removeFromList(_id: string) {
-    return this.userListModel.deleteOne({ _id: new mongoose.Types.ObjectId(_id) }).exec();
-  }
-
-  async listMyItems(userId: string, limit: number, skip: number) {
-    const [list, total] = await Promise.all([
-      this.userListModel.find({ userId: new mongoose.Types.ObjectId(userId) }).limit(limit).skip(skip).exec(),
-      this.userListModel.countDocuments({ userId: new mongoose.Types.ObjectId(userId) })
-    ]);
-
-    return {
-      total,
-      list
+    async removeFromList(_id: string) {
+        return this.userListModel.deleteOne({ _id: new mongoose.Types.ObjectId(_id) }).exec();
     }
-  }
 
-  async listUser(id: string) {
-    return this.userModel.findById(id).select('-password').exec();
-  }
+    async listMyItems(userId: string, limit: number, skip: number) {
+        const [list, total] = await Promise.all([
+            this.userListModel.find({ userId: new mongoose.Types.ObjectId(userId) }).limit(limit).skip(skip).exec(),
+            this.userListModel.countDocuments({ userId: new mongoose.Types.ObjectId(userId) })
+        ]);
+
+        return {
+            total,
+            list
+        }
+    }
+
+    async listUser(id: string) {
+        return this.userModel.findById(id).select('-password').exec();
+    }
 }
